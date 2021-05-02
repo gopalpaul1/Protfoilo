bc61126c6607cb3b4a7f47aa5694e406046ca9a5
Gopal Paul <gopal.kumar.paul1@gmail.com>
2021-05-02 18:14:46 +0600

    protfolio

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faGitAlt, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import {  } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 library.add(fab)
@@ -29,8 +29,9 @@ const Footer = () => {
                 <div>
                     <h3 className="header-text">Contact Me</h3>
                     <div>
-                        <a href="https://github.com/gopalpaul1" target="_blank"><FontAwesomeIcon style={{color:"white", fontSize:"30px",marginRight:"30px"}} icon={fab, faGitAlt} /></a>
-                        <a href="https://www.linkedin.com/in/gopal-paul-ba5050211/" target="_blank"><FontAwesomeIcon style={{color:"cyan", fontSize:"30px"}} icon={fab, faLinkedin} /></a>
+                        <a href="https://github.com/gopalpaul1" target="_blank"><FontAwesomeIcon style={{color:"grey", fontSize:"30px",marginRight:"30px"}} icon={fab, faGitAlt} /></a>
+                        <a href="https://www.linkedin.com/in/gopal-paul-ba5050211/" target="_blank"><FontAwesomeIcon style={{color:"cyan", fontSize:"30px",marginRight:"30px"}} icon={fab, faLinkedin} /></a>
+                        <a href="https://gopal.paul573747@gmail.com" target="_blank"><FontAwesomeIcon style={{color:"white", fontSize:"30px"}} icon={fab, faEnvelope} /></a>
                     </div>
                 </div>
             </div>
